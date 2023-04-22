4ae279c354f1cfef1f8176e5f0101801ddf77ba0
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-04-22 15:14:10 -0700

    Fixed the add_two_numbers function by removing the incorrect condition when num1 is 0 and overwrote the cool_codebase.py file with the fixed code.

--- cool_codebase.py
+++ cool_codebase.py
@@ -2,4 +2,11 @@ def fib(n):
     if n <= 1:
         return n
     else:
-        return fib(n-1) + fib(n-2)
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
+
+
+def add_two_numbers(num1, num2):
+    return num1 + num2
